303f830c0b5a5b9f179a416dd62c3795ab0647db
Puneet Walecha <puwalech@microsoft.com>
2021-07-31 11:02:52 +0530

    Re-Arrange the folder structures. Fix the issue for the projects not appearing in the solution

---
+++ Library/TrendingCalculatorForModelsStrategy/TrendingCalculateForCustomCriteria.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+using TrendsCalculator.Library.AlgoComponents.GlobalZCalculationCriterias;
+
+namespace TrendsCalculator.Library.TrendingCalculatorForModelsStrategy
+{
+    internal class TrendingCalculateForCustomCriteria : BaseTrendingCalculator
+    {
+        internal override IAlgoCriteria GetAlgoConstruct()
+        {
+            return new GlobalZCalculationCustomCriteria();
+        }
+
+        internal override List<T> PostProcessZScore<T>(List<T> trendingModels)
+        {
+            return trendingModels;
+        }
+    }
+}
---
+++ Library/TrendingCalculatorForModelsStrategy/TrendingCalculateForZMeanCriteria.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+using TrendsCalculator.Library.AlgoComponents.GlobalZCalculationCriterias;
+
+namespace TrendsCalculator.Library.TrendingCalculatorForModelsStrategy
+{
+    internal class TrendingCalculateForZMeanCriteria : BaseTrendingCalculator
+    {
+        internal override IAlgoCriteria GetAlgoConstruct()
+        {
+            return new GlobalZCalculationZMeanCriteria();
+        }
+
+        internal override List<T> PostProcessZScore<T>(List<T> trendingModels)
+        {
+            var trendingModelsGlobalZMeanCriteria = new List<T>();
+            double meanGlobalZ = GlobalZCalculationZMeanCriteria.MeanGlobalZ;
+            foreach (T model in trendingModels)
+            {
+                if (model.GlobalZ >= meanGlobalZ)
+                {
+                    trendingModelsGlobalZMeanCriteria.Add(model);
+                }
+            }
+            if (trendingModelsGlobalZMeanCriteria.Count > 1)
+            {
+                SortingGlobalZ<T> sortingGLobalZ = new SortingGlobalZ<T>();
+                trendingModelsGlobalZMeanCriteria.Sort(0, trendingModelsGlobalZMeanCriteria.Count, sortingGLobalZ);
+            }
+            return trendingModelsGlobalZMeanCriteria;
+        }
+    }
+}
